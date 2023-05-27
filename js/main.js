b3c31558184003d1f7f03da94c6ec0b5d2185b03
unknown <eiad.omar88@gmail.com>
2023-05-27 23:49:54 +0300

    added choosing hand functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ rulesBtn = document.querySelector(".rules-btn");
 darkFilter = document.querySelector(".dark-filter");
 rulesBox = document.querySelector(".rules-box");
 closeRulesBtn = document.querySelector(".close-rules");
+handBoxes = document.querySelectorAll(".hand-box");
+pickHandContainer = document.querySelector(".pick-hand");
+let userHand = "";
 
 rulesBtn.addEventListener("click", toggleRules);
 closeRulesBtn.addEventListener("click", toggleRules);
@@ -11,3 +14,12 @@ function toggleRules() {
   darkFilter.classList.toggle("hidden");
   rulesBox.classList.toggle("hidden");
 }
+
+handBoxes.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    pickHandContainer.classList.toggle("collapse");
+    if (ele.classList.contains("paper-box")) userHand = "p";
+    else if (ele.classList.contains("scissors-box")) userHand = "s";
+    else userHand = "r";
+  });
+});
